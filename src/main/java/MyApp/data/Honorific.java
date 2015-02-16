5b177de1747feff6830b5ccef02b4ece59376eab
shamaevbs <shamaev.bs@vologda.softlab.ru>
2015-02-16 14:44:30 +0300

    first commit

---
+++ src/main/java/MyApp/data/Honorific.java
@@ -0,0 +1,12 @@
+package MyApp.data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shamaev.bs
+ * Date: 12.02.15
+ * Time: 17:33
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Honorific {
+    MR, MRS, MISS, DR
+}
