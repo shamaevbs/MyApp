1b81392f71970e3970b285d4fbd2e7f333424e16
shamaevbs <shamaev.bs@vologda.softlab.ru>
2015-02-16 15:22:29 +0300

    first commit

--- src/main/java/MyApp/pages/address/CreateAddress.java
+++ src/main/java/MyApp/pages/address/CreateAddress.java
@@ -1,7 +1,12 @@
 package MyApp.pages.address;
 
 import MyApp.entities.Address;
+import MyApp.pages.Index;
+import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,4 +18,16 @@ import org.apache.tapestry5.annotations.Property;
 public class CreateAddress {
     @Property
     private Address address;
+    @Inject
+    private Session session;
+    @InjectPage
+    private Index index;
+    @CommitAfter
+    Object onSuccess()
+    {
+        session.persist(address);
+
+        return index;
+    }
+
 }
