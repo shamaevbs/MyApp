445e3bcb8162d1c19f9a52f25a1cb5177d5be43c
shamaevbs <shamaev.bs@vologda.softlab.ru>
2015-02-19 18:23:24 +0300

    first commit

--- src/main/java/MyApp/components/Layout.java
+++ src/main/java/MyApp/components/Layout.java
@@ -52,6 +52,6 @@ public class Layout
 
     public String[] getPageNames()
     {
-        return new String[]{"Index", "About", "Contact"};
+        return new String[]{"Pizza", "Index",  "About",  "Book"};
     }
 }
