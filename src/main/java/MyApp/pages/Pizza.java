3cca7d7a430a470e2da49637f5e2c4831b7a325a
shamaevbs <shamaev.bs@vologda.softlab.ru>
2015-02-19 18:24:43 +0300

    first commit

---
+++ src/main/java/MyApp/pages/Pizza.java
@@ -0,0 +1,11 @@
+package MyApp.pages;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shamaev.bs
+ * Date: 19.02.15
+ * Time: 15:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class Pizza {
+}
