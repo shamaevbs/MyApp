30c1effb3ab034b166ea43f39e3a28fadaa90d95
shamaevbs <shamaev.bs@vologda.softlab.ru>
2015-03-19 15:16:41 +0300

    19.03.2015 15:16

--- src/main/java/MyApp/pages/Bid.java
+++ src/main/java/MyApp/pages/Bid.java
@@ -4,8 +4,11 @@ import MyApp.entities.Application;
 import MyApp.entities.Commodity;
 import MyApp.services.WebUser;
 import org.apache.tapestry5.PersistenceConstants;
+import org.apache.tapestry5.annotations.Component;
+import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.hibernate.annotations.CommitAfter;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.hibernate.Session;
@@ -19,6 +22,11 @@ public class Bid
     @Property private Commodity commodity;
     @Inject private Session session;
 
+    @Component(id = "applicationform")
+    private Form form;
+
+
+
     //@InjectPage
     @Property @Persist private long clientID;
     //@Property @Persist private Commodity commodity;
@@ -31,7 +39,6 @@ public class Bid
     @Inject
     WebUser webUser;
 
-
     @Property @Persist(PersistenceConstants.FLASH)
     private String message;
 
@@ -40,15 +47,28 @@ public class Bid
     Object onSuccess()
     {
         session.persist(application);
-        message= String.format(" ");
         return null;
+
+
+        //message = String.format(message+" "+product.getPrice()+ " "+  product.getName()+"," );
     }
+
+
 
     public List<Commodity> getCommodities() {
         return session.createCriteria(Commodity.class).add(Restrictions.eq("client", webUser.getUser())).list();
     }
 
+    @OnEvent(component = "applicationform")
+    Object makeGuess()
+    {
+
+        message = String.format(";");
+        return null;
+    }
+
     void setup(long clientID) {
+        message= String.format(";");
         //this.clientID = clientID;
        /* //Product product = (Product) session.createCriteria(Product.class).add(Restrictions.eq("name", "a")).uniqueResult();
         Product product = (Product) session.get(Product.class, productID);
